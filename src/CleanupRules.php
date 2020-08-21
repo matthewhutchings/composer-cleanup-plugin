930b6b944d608be848a748ad3f6a8040d469a33f
Matthew Hutchings <matthew@fewzen.com>
2020-08-21 09:48:28 +0100

    update

--- src/CleanupRules.php
+++ src/CleanupRules.php
@@ -8,15 +8,16 @@ class CleanupRules
     {
         // Default patterns for common files
         $docs = 'README* CHANGELOG* FAQ* CONTRIBUTING* HISTORY* UPGRADING* UPGRADE* package* demo example examples doc docs readme* changelog* composer*';
-        $tests = '.travis.yml .scrutinizer.yml phpcs.xml* phpcs.php phpunit.xml* phpunit.php test tests Tests travis patchwork.json';
+        $tests = '.travis.yml .scrutinizer.yml phpcs.xml* phpcs.php phpunit.xml* phpunit.php test tests Tests travis patchwork.json .git';
 
         return array(
-            'tecnickcom/tcpdf'                     => array($docs, $tests, 'fonts'),
+            'google/apiclient-services'             => array($docs, $tests, 'src/Google/Compute', 'src/Google/Dfareporting', 'src/Google/ShoppingContent', 'src/Google/YouTube', 'src/Google/DisplayVideo', 'src/Google/Vision', 'src/Google/Apigee', 'src/Google/DLP', 'src/Google/Dialogflow', 'src/Google/Dataflow'),
+            'tecnickcom/tcpdf'                      => array($docs, $tests, 'fonts'),
             'anahkiasen/former'                     => array($docs, $tests),
             'anahkiasen/html-object'                => array($docs, 'phpunit.xml* tests/*'),
             'anahkiasen/rocketeer'                  => array($docs, $tests),
             'anahkiasen/underscore-php'             => array($docs, $tests),
-            'aws/aws-sdk-php'                 	    => array($docs, $tests),
+            'aws/aws-sdk-php'                       => array($docs, $tests),
             'barryvdh/composer-cleanup-plugin'      => array($docs, $tests),
             'barryvdh/laravel-debugbar'             => array($docs, $tests),
             'barryvdh/laravel-ide-helper'           => array($docs, $tests),
@@ -33,7 +34,7 @@ class CleanupRules
             'doctrine/common'                       => array($docs, $tests, 'bin lib/vendor'),
             'doctrine/dbal'                         => array($docs, $tests, 'bin build* docs2 lib/vendor'),
             'doctrine/inflector'                    => array($docs, $tests),
-            'dompdf/dompdf'                         => array($docs, $tests, 'www'),
+            'dompdf/dompdf'                         => array($docs, $tests, 'www', 'lib/fonts'),
             'filp/whoops'                           => array($docs, $tests),
             'guzzle/guzzle'                         => array($docs, $tests),
             'guzzlehttp/guzzle'                     => array($docs, $tests),
@@ -62,7 +63,7 @@ class CleanupRules
             'nikic/php-parser'                      => array($docs, $tests, 'test_old'),
             'oyejorge/less.php'                     => array($docs, $tests),
             'patchwork/utf8'                        => array($docs, $tests),
-            'phenx/php-font-lib'                    => array($docs, $tests. 'www'),
+            'phenx/php-font-lib'                    => array($docs, $tests . 'www'),
             'phpdocumentor/reflection-docblock'     => array($docs, $tests),
             'phpoffice/phpexcel'                    => array($docs, $tests, 'Examples unitTests changelog.txt'),
             'phpoffice/phpspreadsheet'              => array($docs, $tests, 'samples'),
@@ -97,8 +98,8 @@ class CleanupRules
             'venturecraft/revisionable'             => array($docs, $tests),
             'vlucas/phpdotenv'                      => array($docs, $tests),
             'willdurand/geocoder'                   => array($docs, $tests),
-            'willdurand/geocoder'                   => array($docs, $tests),
-            
+            'fzaninotto/faker'                      => array($docs, $tests, 'src/Faker/Provider/nl_BE', 'src/Faker/Provider/fr_FR', 'src/Faker/Provider/cs_CZ', 'src/Faker/Provider/ru_RU', 'src/Faker/Provider/ka_GE'),
+
 
         );
     }
